095f3158cf83835c06c219e23f81f1af4686dcde
Bogs Espinas <jrpespinas@gmail.com>
2021-05-31 21:23:17 +0800

    feat: add permissions

--- library/views.py
+++ library/views.py
@@ -1,13 +1,15 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 
 # Create your views here.
 class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = BookSerializer
 
 
 class BookDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = BookSerializer
