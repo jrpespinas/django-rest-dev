ecf924e4a7aeaa310bacf9d76bc643f95cc8c3dd
Bogs Espinas <jrpespinas@gmail.com>
2021-05-31 21:10:10 +0800

    fix: change BookList to BookDetail

--- library/urls.py
+++ library/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("books/", views.BookList.as_view()),
-    path("books/<int:pk>", views.BookList.as_view()),
+    path("books/<int:pk>", views.BookDetail.as_view()),
 ]
